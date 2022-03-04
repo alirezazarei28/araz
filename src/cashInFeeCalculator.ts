a316122a13d7b0cd2899c76cc326237064cddf72
Alireza <alirezazarei28@gmail.com>
2022-03-04 20:14:06 +0330

    apis for getting fees from server added but not connected

--- src/cashInFeeCalculator.ts
+++ src/cashInFeeCalculator.ts
@@ -1,5 +1,15 @@
 import { Transaction } from "./Types";
 import * as configs from "./configs";
+import axios from "axios";
+
+// TODO: create a hook for this
+const getCacheInFee = async () => {
+  const res = await axios.get(
+    // TODO: move this to env variables
+    "https://developers.paysera.com/tasks/api/cash-in"
+  );
+  return res.data;
+};
 
 const cashInFeeCalculator = (transaction: Transaction) =>
   transaction.operation.amount * configs.CASH_IN_FEE < configs.CASH_IN_FEE_MAX
