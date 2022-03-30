0704d5e7010b9143b46e9a9264954ce662a923cb
Alireza <alirezazarei28@gmail.com>
2022-03-30 17:45:15 +0430

    eslint typescript extensions added

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: false,
     es2021: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
