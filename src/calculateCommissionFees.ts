c1a08d13f2b892b424cafead5d24caab0bb855aa
Alireza <alirezazarei28@gmail.com>
2022-03-31 17:44:42 +0430

    some input validation added

--- src/calculateCommissionFees.ts
+++ src/calculateCommissionFees.ts
@@ -1,16 +1,34 @@
 import { ceilPrecise } from "./utils/ceilPrecise";
+import { isValidDate } from "./utils/isValidDate";
 import { Transaction } from "./Types";
 import { cashInFeeCalculator } from "./cashInFeeCalculator";
 import { cashOutFeeCalculator } from "./cashOutFeeCalculator";
-import { TRANSACTION_TYPE_CASH_IN, TRANSACTION_TYPE_CASH_OUT } from "./configs";
+import {
+  TRANSACTION_TYPE_CASH_IN,
+  TRANSACTION_TYPE_CASH_OUT,
+  USER_TYPE_JURIDICAL,
+  USER_TYPE_NATURAL,
+} from "./configs";
 
 const submittedTransactions: Transaction[] = [];
 const ceilPrecise2 = ceilPrecise(2);
+
+const checkInput = (transaction: Transaction) => {
+  if (
+    transaction.user_type !== USER_TYPE_NATURAL &&
+    transaction.user_type !== USER_TYPE_JURIDICAL
+  ) {
+    throw new Error("Wrong User Type");
+  }
+
+  if (!isValidDate(transaction.date)) throw new Error(" invalid date format");
+};
 
 const calculateCommissionFees = async (data: Transaction[]) => {
   const result: any[] = [];
 
   for (let i = 0; i < data.length; i++) {
+    checkInput(data[i]);
     if (data[i].type === TRANSACTION_TYPE_CASH_IN) {
       result[i] = await cashInFeeCalculator(data[i]);
       submittedTransactions.push(data[i]);
@@ -19,7 +37,6 @@ const calculateCommissionFees = async (data: Transaction[]) => {
       submittedTransactions.push(data[i]);
     } else throw new Error("transaction type is invalid");
   }
-
   result.map((res) => console.log(ceilPrecise2(res)));
 };
 
---
+++ src/utils/isValidDate.ts
@@ -0,0 +1,8 @@
+export const isValidDate = (dateString: string) => {
+  var regEx = /^\d{4}-\d{2}-\d{2}$/;
+  if (!dateString.match(regEx)) return false;
+  var d = new Date(dateString);
+  var dNum = d.getTime();
+  if (!dNum && dNum !== 0) return false;
+  return d.toISOString().slice(0, 10) === dateString;
+};
